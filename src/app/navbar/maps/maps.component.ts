e536b85a714ff9e546f704713e4056b2c3495ce0
kolodxxv <crowcrxst@gmail.com>
2023-08-06 06:14:32 +0300

    FM-004.4 Form controls on map component added

--- src/app/navbar/maps/maps.component.ts
+++ src/app/navbar/maps/maps.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject, pipe, map, ReplaySubject, } from 'rxjs'
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Utils } from 'src/app/utils/utils';
+import { YaReadyEvent } from 'angular8-yandex-maps';
+
 
 
 
@@ -8,6 +13,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./maps.component.scss']
 })
 export class MapsComponent {
+
+  public currentAddres?: any[] = []
+  public map?: ymaps.Map;
+
+ public userInputGroup = new FormGroup({
+  apt: new FormControl(''),
+  int: new FormControl(),
+  stair: new FormControl(''),
+  floor: new FormControl('')
+ })
+
+  constructor() {}
+
+  onSubmit(){
+    this.currentAddres?.push(this.userInputGroup.value)
+    console.log(this.currentAddres)
+  }
+
+
+  
+  
+
+
+  
 
 
 }
