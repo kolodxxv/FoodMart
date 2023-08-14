f19936a7f20825469e38c75f9fb44f40f54a952b
kolodxxv <crowcrxst@gmail.com>
2023-08-14 19:10:19 +0300

    FM-004.7 Added placemark functional to the map component

--- src/app/navbar/maps/maps.component.ts
+++ src/app/navbar/maps/maps.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject} from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { YaReadyEvent, YaGeocoderService, YaEvent } from 'angular8-yandex-maps';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Utils } from 'src/app/utils/utils';
 
 
 
@@ -22,26 +23,41 @@ export class MapsComponent {
 
   public currentAddres?: any[] = []
   public selectedAddress: any;
+  public selectedCoords: any;
+  
+  // Input Group
+ public userInputGroup = new FormGroup({})
 
- public userInputGroup = new FormGroup({
-  apt: new FormControl(''),
-  int: new FormControl(),
-  stair: new FormControl(''),
-  floor: new FormControl(''),
-  comment: new FormControl('')
- })
+//  Placemark settings
+placemarkPropperties: ymaps.IPlacemarkProperties = {
+  hintContent: 'Selected address',
+}
+
+placemarkOptions: ymaps.IPlacemarkOptions = {
+  iconLayout: 'default#image',
+  iconImageHref: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
+  iconImageSize: [32, 32]
+
+}
 
   constructor(
     private yaGeocoderService: YaGeocoderService,
     public dialogRef: MatDialogRef<MapsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public utils: Utils
   ) {}
+
+  ngOnInit(): void {
+    const formControls: string[] = ['apt', 'int', 'stair', 'floor', 'comment'];
+    this.userInputGroup = this.utils.generateFormGroup(formControls)
+  }
 
   onMapClick(e: YaEvent<ymaps.Map>): void {
     const { target, event } = e;
 
     if (!target.balloon.isOpen()) {
       const coords = event.get('coords');
+      this.selectedCoords = coords;
       const addrs = this.yaGeocoderService.geocode(`${coords}`, {
         results: 1
       }).subscribe((result: any) => {
@@ -49,44 +65,16 @@ export class MapsComponent {
         this.selectedAddress = firstGeoObj
       } )
 
-      !target.balloon.open(coords, {
-        contentHeader: 'Selected address:',
-        contentBody: '' + this.selectedAddress
-      });
+    // !target.balloon.open(coords, {
+    //   // contentHeader: 'Selected address:',
+    //   // contentBody: '' + this.selectedAddress
+    // });
     } else {
       target.balloon.close()
     }
   }
 
- onMapReady(event: YaReadyEvent<ymaps.Map>) : void {
-  const map = event.target;
-  ymaps.geolocation
-  .get({
-    provider: 'yandex',
-    mapStateAutoApply: true,
-  })
-  .then((result) => {
-    result.geoObjects.options.set('preset', 
-    'islands#redCircleIcon');
-    result.geoObjects.get(0).properties.set({
-      ballonContentBody: 'My location',
-    });
-    map.geoObjects.add(result.geoObjects);
-  });
-
-  ymaps.geolocation
-  .get({
-    provider: 'browser',
-    mapStateAutoApply: true,
-  })
-  .then((result) => {
-    result.geoObjects.options.set('preset', 
-    'islands#blueCircleIcon');
-    map.geoObjects.add(result.geoObjects);
-  })
- }
-
-  onSubmit(){
+  public onSubmit(){
     this.currentAddres?.push(this.userInputGroup.value, this.selectedAddress)
     this.dialogRef.close({data: this.currentAddres});
   }
