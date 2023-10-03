403ca2f080e301872bf53af058b75bf3cde14f55
kolodxxv <kolodxxv@gmail.com>
2023-10-03 16:42:52 +0300

    FM-006.0 Initial attempt to implement the server side for app

---
+++ laravel/database/migrations/2023_10_03_130450_create_outlets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOutletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outlets', function (Blueprint $table) {
+            $table->id();
+            $table->string('outlet');
+            $table->string('label');
+            $table->string('foodTypeShortLabel');
+            $table->string('foodTypeId');
+            $table->string('img');
+            $table->string('logo');
+            $table->time('hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outlets');
+    }
+}
